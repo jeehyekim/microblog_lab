d09d6ef0cb9894a9a1ec3f9d588209b327d64b91
jeehyekim <iam@jeehyekim.com>
2015-10-16 14:18:05 -0700

    include client-side microblog code

---
+++ public/base.js
@@ -0,0 +1,44 @@
+
+
+$(document).ready(function(){
+	console.log("Sanity Check: JS is working!");
+			
+			var count = 0;
+			var date = new Date();
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			var postTime = hours + ":" + minutes;
+			var month = date.getMonth();
+			var day = date.getDate();
+			var year = date.getFullYear();
+			var postDate =  (month+1) + "/" + day + "/" + year;
+			var heartIcon = "<i class='ion-heart' role='button'>" + count + "</i>";
+
+
+	$('#popover').popover({html:true});
+
+
+
+
+	$('#micro-content').keydown(function(event) {
+		if(event.keyCode == 13) {
+			var count = 0;
+			if ($('#micro-content').val().trim().length > 0) {
+				var counter = 3;
+				var microContent = $('#micro-content').val();
+				var newContent = "<div class='list-group-item' id='post-'" + counter + ">" + microContent + "<br><span id='date'></span></div>";
+				$('#micro-posts').prepend(newContent).each(function(){$('#date',this).html(postDate + "      " + postTime + heartIcon);});
+				$('#micro-content').val('');
+				counter++;
+		$('.list-group-item').on('click',function(e) {
+			$('#date').children().html(count++);
+			
+			// console.log(count++);
+		});
+		return false;
+			}
+		}
+
+	});
+
+});
--- server.js
+++ server.js
@@ -3,9 +3,15 @@ var express = require("express"),
     path = require("path"),
     bodyParser = require("body-parser");
 
+ // var db = require('models');
+
+app.set('view engine', 'ejs');
+app.use("/static", express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', function(req,res){
-	res.send("testing");
+	res.render('index');
 });
 
 
