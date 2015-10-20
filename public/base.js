5f5654d4b568162d4bf1c4004cdccb6e32210a47
jeehyekim <iam@jeehyekim.com>
2015-10-20 13:32:52 -0700

    project commit

--- public/base.js
+++ public/base.js
@@ -3,50 +3,61 @@
 $(document).ready(function(){
 	console.log("Sanity Check: JS is working!");
 			
-			var count = 0;
-			var date = new Date();
-			var hours = date.getHours();
-			var minutes = date.getMinutes();
-			var postTime = hours + ":" + minutes;
-			var month = date.getMonth();
-			var day = date.getDate();
-			var year = date.getFullYear();
-			var postDate =  (month+1) + "/" + day + "/" + year;
-			var microContent;
+			// var count = 0;
+			// var date = new Date();
+			// var hours = date.getHours();
+			// var minutes = date.getMinutes();
+			// var postTime = hours + ":" + minutes;
+			// var month = date.getMonth();
+			// var day = date.getDate();
+			// var year = date.getFullYear();
+			// var postDate =  (month+1) + "/" + day + "/" + year;
+			// var microContent;
 
 	$('#popover').popover({html:true});
 
-	$('#micro-content').keydown(function(event) {
-		// event.preventDefault();
-		if(event.keyCode == 13) {
-			if ($('#micro-content').val().trim().length > 0) {
-				microContent = $('#micro-content').serialize();
-				$('#micro-content').val('');
-				console.log("form submitted");
 
-				$.ajax({
-					url: '/posts',
-					type: "POSTS",
-					data: microContent
-				})
-				.done(function(data) {
-					console.log(microContent);
-				    var newContent = "<li class='list-group-item'>" + microContent + "<span data-id='" + data._id + "' class='close delete'>X</span></li>";
-					    $('.entries').prepend(newContent);
-				})
-				.fail(function(data) {
-					console.log("failed to post");
-				});
-
-		return false;
-			}
-		}
+	$('#newPost').on('submit', function(e) {
+		e.preventDefault();
+		var microContent = $(this).serialize();
+		$('#micro-content').val('');
+		console.log(microContent);
+		$.ajax({
+			url: '/posts',
+			type: "POST",
+			data: microContent
+		})
+		.done(function(data) {
+			console.log("made a new post", data);
+			var newContent = "<li class='list-group-item'>" + data.content + "<span data-id='" + data._id + "' class='close delete'>X</span></li>";
+			$('.entries').prepend(newContent);
+			$('#newPost')[0].reset();
+		})
+		.fail(function(data) {
+			alert("Failed to post");
+			console.log("Failed to post");
+		});
 
 	});
 
+
 	$('.entries').on('click', '.close', function(e) {
 		e.preventDefault();
-		console.log("delete works");
+		var postId = $(this).data().id;
+		var deletedPost = $(this).closest('li');
+		$(deletedPost).remove();
+
+		$.ajax({
+			url: '/posts/' + postId,
+			type: 'DELETE'
+		})
+		.done(function(data) {
+			console.log(data);
+			console.log("Post was deleted");
+		})
+		.fail(function(data){
+			console.log("Failed to deleted post");
+		});
 	});
 
 });
--- server.js
+++ server.js
@@ -12,12 +12,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //index
-app.get('/posts', function(req,res){
+app.get('/', function(req,res){
 	db.Posts.find({}, function(err, posts) {
 		if (err) console.log(err);
 		res.render('index', {posts:posts});
+		console.log(posts);
 	});
 });
+
+//create
+
+app.post ('/posts', function (req,res){
+	console.log(req.body);
+	db.Posts.create({content: req.body.post}, function(err,post) {
+		console.log("post request went through");
+		if(err) console.log(err);
+		res.json(post);
+	});
+});
+
+
+//delete
+
+app.delete('/posts/:_id', function(req, res) {
+    console.log('post id is: ', req.params._id);
+    db.Posts.find({
+        _id: req.params._id
+    }).remove(function(err, post) {
+        console.log("post deleted");
+        res.json("That post is deleted");
+    });
+});
+
+// db.Posts.find({}, function(err, posts){
+// 	posts[0].remove();
+// 	});
+
 //show posts
 app.get('/posts/:id', function(req,res){
 	var post = posts[req.params.id];
@@ -25,26 +55,18 @@ app.get('/posts/:id', function(req,res){
 });
 
  //create
-app.post('/posts', function(req,res){
-	console.log(req.body);
-	var post = req.body;
-	Posts.push(post);
-	db.Posts.create(post, function(err, post) {
-		if (err) console.log(err);
-		res.json(post);
-		console.log("Post request went through");
-	});
-});
-// 
-// app.delete('/posts/:_id', function(req, res) {
-//     console.log('post id is: ', req.params);
-//     db.Posts.find({
-//         _id: req.params._id
-//     }).remove(function(err, kitten) {
-//         console.log("post deleted");
-//         res.json("That post is deleted");
-//     });
+// app.post('/posts', function(req,res){
+// 	console.log(req.body);
+// 	var post = req.body;
+// 	Posts.push(post);
+// 	db.Posts.create(post, function(err, post) {
+// 		if (err) console.log(err);
+// 		res.json(post);
+// 		console.log("Post request went through");
+// 	});
 // });
+
+
 
 app.listen(3000, function (){
   console.log("listening on port 3000");
